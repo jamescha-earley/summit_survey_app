cb583734eeb09845774f3d23b6b4eda9088d4b51
James Cha-Earley <james.cha.earley@gmail.com>
2025-08-15 10:44:37 -0700

    London

--- streamlit_app.py
+++ streamlit_app.py
@@ -301,7 +301,7 @@ elif q_idx == len(questions):
                     cursor = conn.cursor()
                     
                     insert_sql = f"""
-                    INSERT INTO survey_responses_London (
+                    INSERT INTO SURVEY_RESPONSES_LONDON (
                         response_id, timestamp, name, email, result_group
                     )
                     VALUES (
